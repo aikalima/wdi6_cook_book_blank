ec8446ea8ce152d35249d05dbbbd6da49c954973
markusg <mguehrs@gmail.com>
2014-01-26 14:08:45 -0800

    remove routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 CookBook::Application.routes.draw do
-  resources :books, :recipes, :ingredients, :users, :sessions
+  resources :books, :recipes, :ingredients
 
   root to: 'books#index'
 
-  get '/signup' => 'users#new'
-  delete '/signout', to: 'sessions#destroy'
-  get'/signin' => 'sessions#new'
 end
 
