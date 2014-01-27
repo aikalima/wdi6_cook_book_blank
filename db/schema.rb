24f836004c61a13e22c6ea73dfda79de0ae7642e
markusg <mguehrs@gmail.com>
2014-01-27 12:34:15 -0800

    morning session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709223604) do
+ActiveRecord::Schema.define(:version => 20140127191836) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(:version => 20130709223604) do
     t.integer  "book_id"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
